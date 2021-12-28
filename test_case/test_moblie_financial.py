18a1694c42ea7c0b621346cfa0d2ccb3b0aadbd1
ilan_tal <ilan.tal321@gmail.com>
2021-12-28 16:00:44 +0200

    screenshots

--- test_case/test_moblie_financial.py
+++ test_case/test_moblie_financial.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 
 import utilities.commonOps
@@ -6,9 +7,13 @@ from workfolw.work_flow_mobile import WFM
 
 @pytest.mark.usefixtures("init_mobile")
 class TestMobile:
-
+    @allure.title('icons count')
+    @allure.description('this test counts the amount of icons in home page')
     def test01(self):
         assert utilities.commonOps.get_data('icon_count') == str(len(self.home_page.icons_list()))
+
+    @allure.title('Check persentage calculator')
+    @allure.description('this test Check 69% of 420')
     def test02(self):
         mwf=WFM()
         mwf.find_d(self.home_page)
